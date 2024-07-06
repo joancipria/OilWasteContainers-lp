341456db03ce23e6bb4a8212bbe7cbba405d832c
Joan Ciprià <joancipria@protonmail.com>
2024-07-06 10:21:59 +0200

    Implement max cotnainers and uniform crossover

--- genetic_algorithm.py
+++ genetic_algorithm.py
@@ -10,14 +10,18 @@ from utils import get_population_from_polygon
 from data import possible_locations, valencia_region_polygon, get_solution_coords
 
 
-def random_solution(size):
-    """Creates a vector with random 0 and 1 values."""
-    return [random.randint(0, 1) for _ in range(size)]
+def random_solution(size, max_ones):
+    """Creates a vector with random 0 and 1 values, limiting 1 values to max_ones"""
+    solution = [0] * size
+    ones_positions = random.sample(range(size), max_ones)
+    for pos in ones_positions:
+        solution[pos] = 1
+    return solution
 
 
-def generate_random_pop(pop_size, solution_size):
-    """Creates a random vectors population."""
-    return [random_solution(solution_size) for _ in range(pop_size)]
+def generate_random_pop(pop_size, solution_size, max_ones):
+    """Creates a list with random solutions"""
+    return [random_solution(solution_size, max_ones) for _ in range(pop_size)]
 
 
 def fitness(solution):
@@ -58,7 +62,7 @@ def fitness(solution):
 
 
 def select_parents(population, population_fitness, parents_number, tournament_size):
-    """SSelect parents using the tournament strategy."""
+    """Select parents using the tournament strategy."""
     selected_parents = []
     for _ in range(parents_number):
         tournament = random.sample(
@@ -69,16 +73,56 @@ def select_parents(population, population_fitness, parents_number, tournament_si
     return selected_parents
 
 
-def cross_parents(parent1, parent2):
+def cross_parents_one_point(parent1, parent2, max_ones):
     """Cross two parents to generate two children using a crossover point."""
     cross_point = random.randint(1, len(parent1) - 1)
     child1 = parent1[:cross_point] + parent2[cross_point:]
     child2 = parent2[:cross_point] + parent1[cross_point:]
+
+    # Repair solution if it has more 1s than max_ones
+    if child1.count(1) > max_ones:
+        child1 = repair_solution(child1, max_ones)
+    if child2.count(1) > max_ones:
+        child2 = repair_solution(child2, max_ones)
     return child1, child2
 
 
-def mutate_solution(solution, mutation_rate):
+def cross_parents_uniform(parent1, parent2, max_ones):
+    """Cross two parents to generate two children using uniform crossover"""
+    child1 = []
+    child2 = []
+
+    for gen1, gen2 in zip(parent1, parent2):
+        if random.random() < 0.5:
+            child1.append(gen1)
+            child2.append(gen2)
+        else:
+            child1.append(gen2)
+            child2.append(gen1)
+
+    # Repair solution if it has more 1s than max_ones
+    if child1.count(1) > max_ones:
+        child1 = repair_solution(child1, max_ones)
+    if child2.count(1) > max_ones:
+        child2 = repair_solution(child2, max_ones)
+
+    return child1, child2
+
+
+def repair_solution(solution, max_ones):
+    """Repair a solution by ensuring it has no more than max_ones 1s"""
+    while solution.count(1) > max_ones:
+        one_indices = [i for i, gene in enumerate(solution) if gene == 1]
+        solution[random.choice(one_indices)] = 0
+    return solution
+
+
+def mutate_solution(solution, mutation_rate, max_ones):
     """Mutate an solution by randomly changing its genes at a given mutation rate."""
     for i in range(len(solution)):
         if random.random() < mutation_rate:
-            solution[i] = 1 if solution[i] == 0 else 0
+            if solution[i] == 0:
+                if solution.count(1) < max_ones:
+                    solution[i] = 1
+            else:
+                solution[i] = 0
