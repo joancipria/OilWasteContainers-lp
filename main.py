9cccd8b8170af559c0bea4dd5f0c70a1556a8b63
Joan Ciprià <joancipria@protonmail.com>
2024-07-08 14:24:01 +0200

    Add some extra params

--- main.py
+++ main.py
@@ -13,12 +13,15 @@ from loguru import logger
 
 logger.add("./logs/deap_run_{time}.log")
 
-# Parameters
+# --- Parameters ---
 not_feasible_penalty = 99999999
 cxpb = 0.8  # The probability of mating two individuals.
 mutpb = 0.2  # The probability of mutating an individual.
 ngen = 40  # Number of generations
 pop_size = 300
+tournament_size = 3
+indpb_mate = 0.5  # Independent probability for each attribute to be exchanged
+indpb_mutate = 0.05  # Independent probability for each attribute to be flipped.
 
 # Types
 creator.create("FitnessMin", base.Fitness, weights=(-1.0,))
@@ -29,16 +32,20 @@ toolbox = base.Toolbox()
 
 toolbox.register("attr_bool", random.randint, 0, 1)
 toolbox.register(
-    "individual", tools.initRepeat, creator.Individual, toolbox.attr_bool, individual_size
+    "individual",
+    tools.initRepeat,
+    creator.Individual,
+    toolbox.attr_bool,
+    individual_size,
 )
 toolbox.register("population", tools.initRepeat, list, toolbox.individual)
 
 # Operators
 toolbox.register("evaluate", eval_fitness)
 toolbox.decorate("evaluate", tools.DeltaPenality(feasible, not_feasible_penalty))
-toolbox.register("mate", tools.cxUniform, indpb=0.5)
-toolbox.register("mutate", tools.mutFlipBit, indpb=0.05)
-toolbox.register("select", tools.selTournament, tournsize=3)
+toolbox.register("mate", tools.cxUniform, indpb=indpb_mate)
+toolbox.register("mutate", tools.mutFlipBit, indpb=indpb_mutate)
+toolbox.register("select", tools.selTournament, tournsize=tournament_size)
 
 
 def main(pop_size, cxpb, mutpb, ngen):
