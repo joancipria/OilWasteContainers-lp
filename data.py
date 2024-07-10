833e13d98ce8eeedb5602a3f3a4ff8131787f8c1
Joan Ciprià <joancipria@protonmail.com>
2024-07-10 19:37:41 +0200

    Load pre-processed data

--- data.py
+++ data.py
@@ -1,37 +1,22 @@
 import json
-import pandas as pd
 import geopandas as gpd
-from shapely import Point
 
 # Valencia region
 valencia_region_polygon = gpd.read_file("./data/valencia_region.geojson").dissolve()[
     "geometry"
 ][0]
 
-# Load oil containers data
-oil_containers = []
-with open("./data/contenidors-oli-usat-contenedores-aceite-usado.json") as f:
-    oil_containers = json.load(f)
+
+def load_input_data():
+    try:
+        with open("./data/possible_locations.json") as f:
+            possible_locations = json.load(f)
+
+            return possible_locations, len(possible_locations)
+
+    except ValueError as error:
+        print({"error": error})
 
 
-possible_locations = []
-
-# For each container
-for container in oil_containers:
-    # Get location
-    location = [container["geo_point_2d"]["lon"], container["geo_point_2d"]["lat"]]
-
-    # Check if locations is in valencia region
-    if valencia_region_polygon.contains(Point(location)):
-        possible_locations.append(location)
-
-# Calc individual size
-individual_size = len(possible_locations)
-
-
-def get_solution_coords(solution_vector):
-    points = []
-    for i, location in enumerate(possible_locations):
-        if solution_vector[i] == 1:
-            points.append(location)
-    return points
+# Load input data
+possible_locations, individual_size = load_input_data()
