75c19e22c56461f94449241f1e938fe914b5a8f0
Joan Ciprià <joancipria@protonmail.com>
2024-07-16 12:00:02 +0200

    Implement max_population_min_overlap_heuristic

--- heuristics.py
+++ heuristics.py
@@ -1,38 +1,132 @@
-from shapely import to_geojson
+from shapely import to_geojson, Polygon
 from data import possible_locations, individual_size
 from utils import get_isochrone, get_population_from_polygon
 from ga_functions import eval_fitness, max_containers
+import json
 
 # Isochrone range
 isochrone_range = 5  # minutes
 
+
 # For each possible location, create an isochrone and get pop
-points_and_pop = []
-for i, location in enumerate(possible_locations):
-    isochrone = to_geojson(get_isochrone(location, isochrone_range))
-    population = get_population_from_polygon(isochrone)
-    points_and_pop.append({"index": i, "population": population})
+def generate_isochrones():
+    points_and_pop = []
+    for i, location in enumerate(possible_locations):
+        isochrone = get_isochrone(location, isochrone_range)
+        population = get_population_from_polygon(to_geojson(isochrone))
+        points_and_pop.append(
+            {"index": i, "population": population, "isochrone": isochrone}
+        )
 
-# Sort by ascending population
-points_and_pop = sorted(
-    points_and_pop, key=lambda point: point["population"], reverse=True
-)
-
-# Get the first max_containers elements from the sorted list
-active_points = points_and_pop[:max_containers]
-indices = [point["index"] for point in active_points]
+    # Sort by ascending population
+    points_and_pop = sorted(
+        points_and_pop, key=lambda point: point["population"], reverse=True
+    )
+    return points_and_pop
 
 
-# Initialize solution to 0s
-solution = [0] * individual_size
+def max_population_heuristic():
+    """
+    Selects a subset of locations that maximizes population ensuring the number of containers does not exceed max_containers.
+    """
+    # Get population
+    points_and_pop = generate_isochrones()
 
-# Update the positions specified in active_points to 1
-for index in indices:
-    solution[index] = 1
+    # Get the first max_containers elements from the sorted list
+    active_points = points_and_pop[:max_containers]
+    indices = [point["index"] for point in active_points]
 
-# Check constraint and evaluate solution
-if solution.count(1) == max_containers:
-    fitness = eval_fitness(solution)
-    print("Fitness:", fitness)  # (374616,)
-else:
-    print("The solution does not respect the max_containers constraint")
+    # Initialize solution to 0s
+    solution = [0] * individual_size
+
+    # Update the positions specified in active_points to 1
+    for index in indices:
+        solution[index] = 1
+
+    # Check constraint and evaluate solution
+    num_containers = solution.count(1)
+
+    if num_containers == max_containers:
+        fitness = eval_fitness(solution)
+        print("Fitness:", fitness, "Containers: ", num_containers)  # (374616,)
+
+        # Convert and write to json file
+        with open(
+            "./results/max_population_heuristic.json", "w"
+        ) as outfile:
+            json.dump(
+                {
+                    "fitness": fitness,
+                    "num_containers": num_containers,
+                    "solution": solution,
+                },
+                outfile,
+            )
+    else:
+        print("The solution does not respect the max_containers constraint")
+
+
+def max_population_min_overlap_heuristic(threshold=0.56):
+    """
+    Selects a subset of locations that maximizes population while minimizing overlap and
+    ensuring the number of containers does not exceed max_containers.
+    """
+    # Get population
+    points_and_pop = generate_isochrones()
+
+    selected_indices = []
+    selected_polygons = []
+
+    for location in points_and_pop:
+        if len(selected_indices) >= max_containers:
+            break
+
+        new_polygon = location["isochrone"]
+        # Check if the new polygon overlaps significantly with any of the already selected polygons
+        if all(
+            new_polygon.intersection(selected_polygon).area
+            < new_polygon.area * threshold
+            for selected_polygon in selected_polygons
+        ):
+            selected_indices.append(location["index"])
+            selected_polygons.append(new_polygon)
+
+    # Initialize solution to 0s
+    solution = [0] * individual_size
+
+    # Update the positions specified in active_points to 1
+    for index in selected_indices:
+        solution[index] = 1
+
+    # Check constraint and evaluate solution
+    num_containers = solution.count(1)
+
+    if solution.count(1) <= max_containers:
+        fitness = eval_fitness(solution)
+        print(
+            "Fitness:",
+            fitness,
+            "Containers: ",
+            num_containers,
+            "Overlap threshold: ",
+            threshold,
+        )  # (260021,)
+
+        # Convert and write to json file
+        with open(
+            "./results/max_population_min_overlap_heuristic.json", "w"
+        ) as outfile:
+            json.dump(
+                {
+                    "fitness": fitness,
+                    "num_containers": num_containers,
+                    "overlap_threshold": threshold,
+                    "solution": solution,
+                },
+                outfile,
+            )
+    else:
+        print("The solution does not respect the max_containers constraint")
+
+max_population_heuristic()
+max_population_min_overlap_heuristic()
