094c8ac12a4651e9e5c1c344fc1cea1826a7895f
Joan Ciprià <joancipria@protonmail.com>
2024-07-18 12:46:12 +0200

    Fix batch.sh

--- batch.sh
+++ batch.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
-#SBATCH --cpus-per-task=32 --mem=92160
+#SBATCH --cpus-per-task=1
+#SBATCH --ntasks=10 
+#SBATCH --mem=30720
+
+# Activate virtual env
 source ./venv/bin/activate
-for ((i=1; i<=30; i++))
-do
-    source ./venv/bin/activate && python main.py "$i" &
+
+# Run in parallel
+for i in {1..10}; do
+    srun --exclusive -N1 -n1 python main.py $i &
 done
+
+# Wait for all background jobs to finish
+Wait
+
+# Deactivate virtual env
+deactivate
